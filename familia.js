39b2c4b7fde7adef3f2ae87abef74ad07c30b67b
EnzoFurtuoso <fivetestedev@gmail.com>
2025-04-18 21:25:40 -0400

    Ajustando as imagens

---
+++ familia.js
@@ -0,0 +1,64 @@
+export const familia = [
+    {
+        nome: "José(Beto)",
+        foto: './img/Beto.jpg',
+        netos: [
+            { nome: 'Tatiane', foto: './img/Tati.jpg' },
+            { nome: 'Bruno', foto: './img/Bruno.jpg'}
+        ]
+    },
+
+    {
+        nome: "Aparecido(Tim)",
+        foto: './img/tiotim.jpeg',
+        netos: [
+            { nome: 'Aparecido(Cido)', foto: './img/cido.png' },
+        ]
+    },
+
+    {
+        nome: "Maria Aparecida(Tânia)",
+        foto: './img/Tania.jpg',
+        netos: [
+            { nome: 'Enzo', foto: './img/Enzo.jpg' },
+            { nome: 'Fagner', foto: './img/Fagner.jpg' },
+            { nome: 'Venildo', foto: './img/Venildo.jpg' },
+        ]
+    },
+
+    {
+        nome: "Maria do Socorro(Teia)",
+        foto: './img/Teia.jpg',
+        netos: [
+            { nome: 'Anderson', foto: './img/anderso,.jpeg' },
+            { nome: 'Leonardo', foto: './img/leo.png' }
+        ]
+    },
+
+    {
+        nome: "Célia",
+        foto: './img/Celia.jpg',
+        netos: [
+            { nome: 'Carlos Eduardo', foto: './img/Dudu.jpg' },
+            { nome: 'Celine Mariane', foto: './img/Celine.jpg'}
+        ]
+    },
+
+    {
+        nome: "Sergio",
+        foto: './img/Sergio.jpg',
+        netos: [
+            { nome: 'Luiz Fabiano', foto: './img/Fabiano.jpg' },
+            { nome: 'Yasmin', foto: './img/yasmim.jpg',}
+        ]
+    },
+
+    {
+        nome: "Josias(Bim)",
+        foto: './img/tiobim.jpg',
+        netos: [
+            { nome: 'Guilherme', foto: './img/Gui.jpg' },
+        ]
+    },
+   
+];
